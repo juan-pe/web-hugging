cf97223525649749661e8d53901c95261599f016
Alba Vilardebò <albavilardebo@gmail.com>
2016-01-17 21:00:30 +0100

    Página en construcción

---
+++ common/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+
+
+def home(request):
+	return render(request, 'common/home.html')
+
+
+def faq(request):
+	return render(request, 'common/faq.html')
---
+++ web_hugging/settings/prod.py
@@ -0,0 +1,27 @@
+import json
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+with open(os.getenv('WEB_CONFIG'), 'r') as f:
+	bd_config = json.loads(f.read())
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': bd_config['name'],
+        'USER': bd_config['user'],
+        'PASSWORD': bd_config['password'],
+        'HOST':'localhost',
+        'PORT':'5433',
+    }
+}
+
+INSTALLED_APPS += [
+    'common',
+]
