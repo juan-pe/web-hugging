06253444051854b392925434770b4cd1af86eabf
Juan Pedro Sánchez <jsanchez@gaa9.com>
2016-01-19 23:02:35 +0100

    static urls

--- web_hugging/urls.py
+++ web_hugging/urls.py
@@ -18,10 +18,14 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from common import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^faq', views.faq, name='faqs')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
