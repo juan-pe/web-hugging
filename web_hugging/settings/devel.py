ccc9c6012f054547c212adce4d76c46274487b04
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2016-04-10 15:35:45 +0200

    Security issues + improve settings

--- web_hugging/settings/devel.py
+++ web_hugging/settings/devel.py
@@ -8,24 +8,6 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = []
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-with open(os.getenv('WEB_CONFIG'), 'r') as f:
-    bd_config = json.loads(f.read())
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': bd_config['name'],
-        'USER': bd_config['user'],
-        'PASSWORD': bd_config['password'],
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
-
 # Application definition
 
 INSTALLED_APPS += [
--- web_hugging/settings/prod.py
+++ web_hugging/settings/prod.py
@@ -12,24 +12,6 @@ ALLOWED_HOSTS = ['bowie',
                  'huggingnepal.org',
                  'test.huggingnepal.org']
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-with open(os.getenv('WEB_CONFIG'), 'r') as f:
-    bd_config = json.loads(f.read())
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': bd_config['name'],
-        'USER': bd_config['user'],
-        'PASSWORD': bd_config['password'],
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 INSTALLED_APPS += [
     # django-filer
     'easy_thumbnails',
